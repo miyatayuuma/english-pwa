2a6cddf6988fbafdcc641cb2b0da34e0bba433ca
miyatayuuma <115095547+miyatayuuma@users.noreply.github.com>
2025-10-12 00:07:18 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="shell-v16";
+const CACHE="shell-v17";
 const ASSETS=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(
